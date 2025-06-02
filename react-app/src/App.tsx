8ac812d47c89e92e859a30e3bba0b260732badd2
robertbrada <bert.blader@gmail.com>
2025-06-02 18:21:00 +0200

    feat: read shared env variables

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,3 +1,15 @@
+const aztecNodeUrl = import.meta.env.AZTEC_NODE_URL;
+const contractAddress = import.meta.env.CONTRACT_ADDRESS;
+const deployerAddress = import.meta.env.DEPLOYER_ADDRESS;
+const deploymentSalt = import.meta.env.DEPLOYMENT_SALT;
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <div>{aztecNodeUrl}</div>
+      <div>{contractAddress}</div>
+      <div>{deployerAddress}</div>
+      <div>{deploymentSalt}</div>
+    </div>
+  );
 }
--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '../', '');
+
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.CONTRACT_ADDRESS': JSON.stringify(env.CONTRACT_ADDRESS),
+      'import.meta.env.DEPLOYER_ADDRESS': JSON.stringify(env.DEPLOYER_ADDRESS),
+      'import.meta.env.DEPLOYMENT_SALT': JSON.stringify(env.DEPLOYMENT_SALT),
+      'import.meta.env.AZTEC_NODE_URL': JSON.stringify(env.AZTEC_NODE_URL),
+    },
+  };
+});
